f59e37c50f4f580040031c482b1efe0c161e28e6
nbukrieiev <nikita.bukrieiev@cengage.com>
2019-11-14 10:10:31 +0200

    Initial commit

---
+++ src/test/java/com/gmail/buer2012/compiler/CompilerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.buer2012.compiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompilerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
