1a19b2ea792ac30ba291152660dd6fbcf390e980
BukerKo <buer2012@gmail.com>
2019-12-24 00:24:57 +0200

    Just triggering the build

--- src/main/java/com/gmail/buer2012/utils/ErrorUtils.java
+++ src/main/java/com/gmail/buer2012/utils/ErrorUtils.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class ErrorUtils {
-    
     public static List<String> getErrorMessages(List<Diagnostic<? extends JavaFileObject>> diagnostics) {
         Diagnostic<? extends JavaFileObject> diagnostic = diagnostics.get(diagnostics.size() -1 );
         return Collections.singletonList(String.format("%s on line %d in column %d:\n", diagnostic.getKind(), diagnostic.getLineNumber(), diagnostic.getColumnNumber())
