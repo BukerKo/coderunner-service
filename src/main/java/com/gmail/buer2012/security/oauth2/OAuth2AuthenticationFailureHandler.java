6b5141cab85b0e8895dd715054e77ebb3a42e1a8
Михаил Грошевой <developer.misha999777@gmail.com>
2020-01-09 22:33:37 +0200

    Debug

--- src/main/java/com/gmail/buer2012/security/oauth2/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/com/gmail/buer2012/security/oauth2/OAuth2AuthenticationFailureHandler.java
@@ -24,7 +24,7 @@ public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationF
     
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        System.out.println("some failure");
+        System.out.println(exception.getMessage());
         String targetUrl = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
                 .map(Cookie::getValue)
                 .orElse(("/"));
