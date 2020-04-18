344c8698e1f50434857ea432b4e2190df97abc69
Михаил Грошевой <developer.misha999777@gmail.com>
2020-04-18 23:22:27 +0300

    Added paging and search for RunInformation (#44)

--- src/main/java/com/gmail/buer2012/repository/RunInformationRepository.java
+++ src/main/java/com/gmail/buer2012/repository/RunInformationRepository.java
@@ -1,10 +1,12 @@
 package com.gmail.buer2012.repository;
 
 import com.gmail.buer2012.entity.RunInformation;
-import org.springframework.data.repository.CrudRepository;
-
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface RunInformationRepository extends CrudRepository<RunInformation, Long> {
+public interface RunInformationRepository extends PagingAndSortingRepository<RunInformation, Long> {
     Optional<RunInformation> findByUserId(Long userId);
+    Page<RunInformation> findByUser_UsernameContainingIgnoreCase(Pageable page, String user_username);
 }
